0f2b26348976eb973bf717b64cc9eae8ac753cee
BDR <bdr@bdr-VirtualBox.(none)>
2013-04-11 23:55:29 -0400

    learning more about the Exif object and mongodb

--- testexif.js
+++ testexif.js
@@ -2,10 +2,18 @@ var ExifImage = require('exif').ExifImage;
 
 try {
     new ExifImage({ image : '2.jpg' }, function (error, image) {
-        if (error)
+        if (error){
             console.log('Error: '+error.message);
-        else
-            console.log(image); // Do something with your data!
+       } else{
+            console.log(image);
+            console.log("-------------------------------");
+
+            console.log(image.gps[5].tagName); // Do something with your data!
+            console.log(image.gps[5].value);
+             
+	    console.log("_______________________________");
+            console.log("-------------------------------");
+            }
 
     });
 } catch (error) {
